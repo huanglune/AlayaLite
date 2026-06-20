a5efb8ba23e930ee334b9ae6104ab2a9adb014dd
huangliang <huanglune@foxmail.com>
2026-06-20 09:08:34 -0700

    chore(release): exclude version fallback from coverage

--- python/src/alayalite/__init__.py
+++ python/src/alayalite/__init__.py
@@ -45,5 +45,5 @@ try:
     from importlib.metadata import version as _pkg_version
 
     __version__ = _pkg_version("alayalite")
-except PackageNotFoundError:
+except PackageNotFoundError:  # pragma: no cover
     __version__ = "0.0.0.dev"
