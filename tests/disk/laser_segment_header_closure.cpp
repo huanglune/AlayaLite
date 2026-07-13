9dc97d83060595a937c927634e7ee54faec0690a
huangliang <huanglune@foxmail.com>
2026-07-12 18:19:13 -0700

    test(disk): cover LASER-disabled segment factory

--- tests/disk/laser_segment_header_closure.cpp
+++ tests/disk/laser_segment_header_closure.cpp
@@ -3,4 +3,11 @@
 
 #include "index/disk/laser_segment.hpp"
 
-int main() { return 0; }
+int main() {
+  static_assert(!alaya::core::Saveable<alaya::disk::LaserSegment>);
+  alaya::core::OpenContext context;
+  const auto opened = alaya::disk::LaserSegmentFactory::open(alaya::core::ArtifactView{},
+                                                             alaya::core::OpenOptions{},
+                                                             context);
+  return !opened.ok() && opened.status().code() == alaya::core::StatusCode::not_supported ? 0 : 1;
+}
